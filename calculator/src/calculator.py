64b81082b34d011f1769b7930e685b8b5577b8a1
Andres Kessler <andresgabriel.kessler@gmail.com>
2024-08-25 18:22:48 -0300

    setup del programa principal (sin implementar operaciones)

---
+++ calculator/src/calculator.py
@@ -0,0 +1,11 @@
+def calculate(a, b, op):
+    if op == "+":
+        print("Sin implementar")
+    elif op == "-":
+        print("Sin implementar")
+    elif op == "*":
+        print("Sin implementar")
+    elif op == "/":
+        print("Sin implementar")
+    else:
+        raise(ValueError(f"Operador no valido: {op}"))
