2442ddd4c7fa2dde6c0cad21985386912760f83b
Agustina Cabello <cabelloagus000@gmail.com>
2024-08-28 21:56:56 -0300

    Funcionalidad suma arreglada

--- calculator/src/calculator.py
+++ calculator/src/calculator.py
@@ -1,6 +1,9 @@
+from calculator.src import operations
+
+
 def calculate(a, b, op):
     if op == "+":
-        print("Sin implementar")
+        return operations.suma(a,b)
     elif op == "-":
         print("Sin implementar")
     elif op == "*":
