16f1326ea482a2a0c4f6a6b07f6ec70969f99df2
Augusto Conti <contiaugusto07@gmail.com>
2024-08-30 14:58:58 -0300

    Agregué la funcionalidad para dividir con excepciones

--- calculator/main.py
+++ calculator/main.py
@@ -4,8 +4,11 @@ def main():
     x = float(input("Ingrese el primer numero: "))
     operador = input("Ingrese el operador (+, -, *, /): ")
     y = float(input("Ingrese el segundo numero: "))
-    result = calc.calculate(x,y,operador)
-    print(f'El resultado es: {result}')
+    try:
+        result = calc.calculate(x,y,operador)
+        print(f'El resultado es: {result}')
+    except ZeroDivisionError:
+        print("No se puede dividir por 0")
 
 if __name__ == "__main__": 
     main()
