8db6d8eaaf6e99b41089f6dfe5a21dd37d83d59e
Matias Cuacci <matiascuacci7@gmail.com>
2024-09-05 10:17:06 -0300

    Agregue html y css vacio

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
-def create_app(env="development", static_folder=""):
+from flask import render_template
+
+def create_app(env="development", static_folder="../../static"):
     app = Flask(__name__)
     @app.route("/")
     def home():
         return "Bueeeenas"
-    
     return app
