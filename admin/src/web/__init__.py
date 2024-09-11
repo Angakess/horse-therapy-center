3dc0ef723120b1800d990888d7114d2ba0560344
Andres Kessler <andresgabriel.kessler@gmail.com>
2024-09-11 10:42:54 -0300

    agrego el layout template

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask import render_template
+from flask import url_for
+
 
 def create_app(env="development", static_folder="../../static"):
     app = Flask(__name__)
     @app.route("/")
     def home():
-        return "Bueeeenas"
+        #Cambiar por el home.html
+        return render_template('layout.html')
     return app
