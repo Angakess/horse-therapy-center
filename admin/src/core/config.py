1a4d7a8ca4e5a6cbd671199232a732d6ea4a2e3e
Andres Kessler <andresgabriel.kessler@gmail.com>
2024-09-24 14:51:38 -0300

    Configuracion de la DB (desarrollo)

---
+++ admin/src/core/config.py
@@ -0,0 +1,25 @@
+class Config(object):
+    TESTING = False
+
+class ProductionConfig(Config):
+    pass
+
+class DevelopmentConfig(Config):
+    DB_USER = "postgres"
+    DB_PASSWORD = "postgres"
+    DB_HOST = "localhost"
+    DB_PORT = "5432"
+    DB_NAME = "grupo28"
+    SQLALCHEMY_DATABASE_URI = (
+        f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    )
+    
+
+class TestingConfig(Config):
+    TESTING = True
+
+config = {
+    "development": DevelopmentConfig,
+    "production": ProductionConfig,
+    "testing": TestingConfig,
+}
--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -1,15 +1,22 @@
 from flask import Flask
 from flask import render_template
 from flask import url_for
-from src.web.helpers import handler
-from src.web.config import config
-from src.web.controllers.issues import bprint as issues_bp
+from web.helpers import handler
+from web.config import config
+from web.controllers.issues import bprint as issues_bp
+from core import database
+from core import seeds
+from core.config import config
+
 
 def create_app(env="development",  static_folder="../../static"):
     app = Flask(__name__, static_folder=static_folder)
+
     app.config.from_object(config[env])
     print(app.config)
     
+    database.init_app(app)
+
     @app.route("/")
     def home():
         return render_template("home.html")
@@ -23,5 +30,15 @@ def create_app(env="development",  static_folder="../../static"):
     
     # Error handlers
     app.register_error_handler(404, handler.not_found_error)
+
+
+    # Comandos personalizados
+    @app.cli.command(name="reset-db")
+    def reset_db():
+        database.reset()
+
+    @app.cli.command(name="seeds-db")
+    def seeds_db():
+        seeds.run()
 
     return app
