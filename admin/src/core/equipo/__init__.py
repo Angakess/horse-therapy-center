0b379de9d33e5c18b512b9bb4e8567f3d02e707c
Andres Kessler <andresgabriel.kessler@gmail.com>
2024-10-02 09:18:24 -0300

    Cambio de nombre de la carpeta board a equipo

--- admin/src/core/equipo/__init__.py
+++ admin/src/core/equipo/__init__.py
@@ -1,16 +1,6 @@
 from core.database import db
-from core.board.equipos import Equipo
+from core.equipo.equipos import Equipo
 
-
-def list_issues():
-    issues = [
-        {
-        "id": 1,
-        "title": "Issue 1",
-        "status": "Nuevo",
-        }
-    ]
-    return issues
 
 def list_equipos():
     equipos = Equipo.query.all()
