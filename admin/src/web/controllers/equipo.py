e9b25d960fcd35f597ea55398dcd21bca9c81349
Andres Kessler <andresgabriel.kessler@gmail.com>
2024-10-04 17:45:27 -0300

    Funcion de ordenar lista segun nombre, apellido y fecha de creacion

--- admin/src/web/controllers/equipo.py
+++ admin/src/web/controllers/equipo.py
@@ -9,6 +9,9 @@ bprint = Blueprint("equipo", __name__, url_prefix="/equipo")
 def index():
     equipos = equipo.list_equipos()
     query = request.args.get('query','')
+    order = request.args.get('order', 'asc')
+    by = request.args.get('by','')
+
     if query:
         equipos = [
             equipo for equipo in equipos if (
@@ -20,15 +23,23 @@ def index():
             )
         ]
     
-    #probablemente borrar despues
-    equipos.sort(key=lambda x: x.id)
+    if by == 'nombre':
+        equipos.sort(key=lambda x: x.nombre, reverse=order == 'asc')
+    elif by == 'apellido':
+        equipos.sort(key=lambda x: x.apellido, reverse=order == 'asc')
+    elif by == 'fecha':
+        equipos.sort(key=lambda x: x.inserted_at, reverse=order == 'asc')
+    else:
+        equipos.sort(key=lambda x: x.id)
 
-    return render_template("equipo/index.html", equipos=equipos, parametro=query)
+    return render_template("equipo/index.html", equipos=equipos, parametro=query, order=order, by=by)
 
 @bprint.post("/toggle-active")
 def toggle_activate():
     chosen_id = request.form['id']
     query = request.form['query']   #guardo lo que haya en la barra de busqueda para no resetearla
+    order = request.form['order']
+    by = request.form['by']
     equipo.toggle_a(chosen_id)
 
-    return redirect(url_for('equipo.index', query=query))
+    return redirect(url_for('equipo.index', query=query, order=order, by=by))
