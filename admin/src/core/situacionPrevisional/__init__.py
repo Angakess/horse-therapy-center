f41f915c4b122ddc2b89c818706eac010fe52909
Matias Cuacci <matiascuacci7@gmail.com>
2024-10-15 08:44:27 -0300

    Blueprints sin archivos JYA/Ecuestre

--- admin/src/core/situacionPrevisional/__init__.py
+++ admin/src/core/situacionPrevisional/__init__.py
@@ -14,3 +14,17 @@ def create_situacion_previsional(**kwargs):
     db.session.add(situacion_previsional)
     db.session.commit()
     return situacion_previsional
+
+def get_situacion_previsional(id):
+    choosed_situacion_previsional = Situacion_previsional.query.filter_by(id=id).first()
+    if not choosed_situacion_previsional:
+        raise ValueError("No se encontró la situación previsional seleccionada")
+    return choosed_situacion_previsional
+
+def delete_situacion_previsional(id):
+    situacion_previsional = Situacion_previsional.query.get(id)
+    if situacion_previsional:
+        db.session.delete(situacion_previsional)
+        db.session.commit()
+    else:
+        raise ValueError("No se encontro la situación previsional a borrar")
