892e0b9df4f219f02abbc1b984e37cee72937687
Matias Cuacci <matiascuacci7@gmail.com>
2024-11-20 14:39:30 -0300

    Cambiando cosas del template

--- portal/src/App.vue
+++ portal/src/App.vue
@@ -5,14 +5,14 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="Cedica logo" class="logo" src="@/assets/cedica.png" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Bienvenidos al CEDICA" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/">Inicio</RouterLink>
+        <RouterLink to="/contacto">Contacto</RouterLink>
       </nav>
     </div>
   </header>
