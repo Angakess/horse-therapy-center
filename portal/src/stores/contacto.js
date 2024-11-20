846e50bbb85a37f61853116b8859659bedee116b
Matias Cuacci <matiascuacci7@gmail.com>
2024-11-20 18:32:13 -0300

    Agrego captcha

--- portal/src/stores/contacto.js
+++ portal/src/stores/contacto.js
@@ -14,9 +14,19 @@ export const useContactoStore = defineStore("contacto", {
       try {
         this.loading = true;
         this.error = null;
+
+        const captchaResponse = grecaptcha.getResponse();
+
+        if (captchaResponse.lenght === 0){
+          this.error = "Captcha no resuelto";
+          this.loading = false;
+          return;
+        }
+
         const response = await axios.post("http://localhost:5000/api/contacto/messages",formData);
         // Agregar el nuevo mensaje a la lista
         this.contacto.push(response.data);
+
       } catch (error) {
         this.error =
           error.response?.data?.message || "Ocurrió un error al enviar el mensaje.";
